ab730704f1fb83bc2c074397010a25f94cd98cc2
Velislav Mutafchiyski <velislav.mu@gmail.com>
2024-11-02 20:10:19 +0200

    Student Controller With View Added

---
+++ StudentManagementSystem.Core/Models/Student/StudentDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using StudentManagementSystem.Infrastructure.Data.Models;
+
+namespace StudentManagementSystem.Core.Models.Student
+{
+    public class StudentDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string PersonalId { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public string ContactDetails { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string CoursesEnrolled { get; set; } = null!;
+        public string Performance { get; set; }
+
+        public ICollection<Grade> Grades { get; set; } = new List<Grade>();
+    }
+}
---
+++ StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Core.Contracts;
+using StudentManagementSystem.Core.Models.Student;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IStudentService studentService;
+
+        public StudentController(IStudentService _studentService)
+        {
+            studentService = _studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id == 0) 
+            { 
+
+            }
+            var model = new StudentDetailsViewModel();
+            return View();
+        }
+    }
+}
