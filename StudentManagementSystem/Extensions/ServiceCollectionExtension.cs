ee7577a3db531ce83bdd55c4f5016551ab49b724
VelkoTgr6 <velislav.mu@gmail.com>
2025-11-17 15:04:01 +0200

    Configure for Render + Supabase deployment

--- StudentManagementSystem/Extensions/ServiceCollectionExtension.cs
+++ StudentManagementSystem/Extensions/ServiceCollectionExtension.cs
@@ -34,27 +34,32 @@ namespace Microsoft.Extensions.DependencyInjection
             if (string.IsNullOrEmpty(connectionString))
             {
                 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
                 if (string.IsNullOrEmpty(databaseUrl))
                 {
                     throw new InvalidOperationException("DATABASE_URL environment variable is not set.");
                 }
 
-                var uri = new Uri(databaseUrl);
-                var userInfo = uri.UserInfo.Split(':');
+                if (databaseUrl.StartsWith("postgres://", StringComparison.OrdinalIgnoreCase))
+                {
+                    var uri = new Uri(databaseUrl);
+                    var userInfo = uri.UserInfo.Split(':');
 
-                var host = uri.Host;
-                var port = uri.Port != -1 ? uri.Port : 5432;
-                var database = uri.AbsolutePath.TrimStart('/');
-                var username = userInfo[0];
-                var password = userInfo.Length > 1 ? userInfo[1] : "";
+                    var host = uri.Host;
+                    var port = uri.Port != -1 ? uri.Port : 5432;
+                    var database = uri.AbsolutePath.TrimStart('/');
+                    var username = userInfo[0];
+                    var password = userInfo.Length > 1 ? userInfo[1] : "";
 
-                connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+                    connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 
-                Console.WriteLine($"Using DATABASE_URL with host {host} and database {database}");
-            }
-            else
-            {
-                Console.WriteLine("Using connection string from configuration");
+                    Console.WriteLine($"Using DATABASE_URL with host {host} and database {database}");
+                }
+                else
+                {
+                    connectionString = databaseUrl;
+                    Console.WriteLine("Using connection string from configuration");
+                }
             }
 
             services.AddDbContext<StudentManagementDbContext>(options =>
@@ -68,8 +73,6 @@ namespace Microsoft.Extensions.DependencyInjection
 
             return services;
         }
-
-
 
         public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
         {
